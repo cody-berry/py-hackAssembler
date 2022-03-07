4eb8e990a131ee0570f9df3770536258b7b02aea
Cody <cody.tian@icloud.com>
2022-03-07 11:12:20 -0800

    add asm files. add template project. add basic coding plan

---
+++ assembler.py
@@ -0,0 +1,11 @@
+# @author cody
+# @date 2022.03.07
+#
+# coding plan
+# 	copy asm files over
+# 	read a file → output same file
+# 		use f-strings, e.g. print(f"{line}")
+# 	ignore whitespace
+# 	ignore full-line comments
+# 	ignore mid-line comments
+
